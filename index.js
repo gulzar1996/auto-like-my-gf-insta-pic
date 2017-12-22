5ce8e2d8b79f90d680e672e2561ca3a3c1ae816f
VinayakBagaria <workvinayak95@gmail.com>
2017-12-22 14:21:00 +0530

    added eslint airbnb styles, code formatted

--- index.js
+++ index.js
@@ -1,85 +1,65 @@
 require('dotenv').config();
 
-var accessToken = process.env.YOUR_INSTAGRAM_ACCESS_TOKEN;
-const user_id = process.env.USER_ID_TO_MONITOR;
-var slack_url = process.env.SLACK_INCOMING_WEBHOOK_URL;
+const accessToken = process.env.YOUR_INSTAGRAM_ACCESS_TOKEN;
+const userID = process.env.USER_ID_TO_MONITOR;
+const slackURL = process.env.SLACK_INCOMING_WEBHOOK_URL;
 
-var InstagramAPI = require('instagram-api');
-var request = require('request');
-var instagramAPI = new InstagramAPI(accessToken);
-var request = require('request');
-const express = require('express')
-const app = express()
+const InstagramAPI = require('instagram-api');
+const request = require('request');
 
+const instagramAPI = new InstagramAPI(accessToken);
+const express = require('express');
 
-app.get('/run', function (req, res)
-{
-    instagramAPI.userMedia(user_id).then(result =>
+const app = express();
+
+function sendPostToSlack(media) {
+  let caption;
+  if (media.caption === null) { caption = 'Unknown'; } else { caption = media.caption.text; }
+  request.post(
+    slackURL,
     {
-        var pics = [];
-        var likePromise = [];
-        result.data.forEach(function(media){
-
-        if (media.user_has_liked == false)
-            likePromise.push(instagramAPI.postMediaLike(media.id).then(resp =>
-            {
-                return sendPostToSlack(media);
-            }));
-        });
-
-        Promise.all(likePromise).then(values =>
-            {
-                console.log('All photos liked')
-                res.send(values);
-            },
-            function(err)
-            {
-                res.send(err);
-            }
-        );
-
+      json: {
+        attachments: [
+          {
+            pretext: caption,
+            image_url: media.images.standard_resolution.url,
+            footer: `Instagram ${media.type} liked`,
+            footer_icon: 'https://lh3.googleusercontent.com/aYbdIM1abwyVSUZLDKoE0CDZGRhlkpsaPOg9tNnBktUQYsXflwknnOn2Ge1Yr7rImGk=w300',
+            ts: Math.floor(Date.now() / 1000),
+          },
+        ],
+      },
     },
-    function(err)
-    {
-        res.send(err);
-        console.log(err); // error info
-    });
+    (error, response) => response,
+  );
+}
 
-})
+
+app.get('/run', (req, res) => {
+  instagramAPI.userMedia(userID).then(
+    (result) => {
+      const likePromise = [];
+      result.data.forEach((media) => {
+        if (media.user_has_liked === false) {
+          likePromise.push(instagramAPI.postMediaLike(media.id).then(() =>
+            sendPostToSlack(media)));
+        }
+      });
+
+      Promise.all(likePromise).then(
+        (values) => {
+          res.send(values);
+        },
+        (err) => {
+          res.send(err);
+        },
+      );
+    },
+    (err) => {
+      res.send(err);
+    },
+  );
+});
 
 app.set('port', (process.env.PORT || 5000));
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
-  });
-
-
-function sendPostToSlack(media)
-{
-    let caption;
-    if(media.caption === null)
-        caption = 'Unknown';
-        else
-        caption = media.caption.text;
-request.post(
-        slack_url,
-        {
-             json: {
-                "attachments": [
-                    {
-                        "pretext": caption,
-                        "image_url": media.images.standard_resolution.url,
-                        "footer": "Instagram "+media.type+" liked",
-                        "footer_icon": "https://lh3.googleusercontent.com/aYbdIM1abwyVSUZLDKoE0CDZGRhlkpsaPOg9tNnBktUQYsXflwknnOn2Ge1Yr7rImGk=w300",
-                        "ts": Math.floor(Date.now() / 1000)
-                    }
-                ]
-            }
-    },
-        function (error, response, body) {
-            return response;
-            if (!error && response.statusCode == 200) {
-                console.log(body)
-            }
-        }
-    );
-}
+app.listen(app.get('port'));
